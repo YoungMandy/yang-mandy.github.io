da994379f526ce0fdddad4e7d60d7ef8a2fc6c63
huili.yang <huili.yang@qq.com>
2023-07-11 18:16:37 +0800

    布局方式

--- src/components/cssbasic/cssLayout.vue
+++ src/components/cssbasic/cssLayout.vue
@@ -1,4 +1,6 @@
 <script setup lang="ts" >
+import { ref } from 'vue';
+let activeKey = ref('table');
 </script>
 <template>
   <section class="component__css-layout">
@@ -12,6 +14,20 @@
         <li>grid布局</li>
       </ul>
     </p>
+      <a-collapse v-model:activeKey="activeKey">
+        <a-collapse-panel key="table" header="table布局" >
+          table布局在等高退拽的时候有妙用
+        </a-collapse-panel>
+        <a-collapse-panel key="float" header="div + float布局" :forceRender="true">
+          div+ float布局
+        </a-collapse-panel>
+        <a-collapse-panel key="flex" header="flex布局">
+          flex是目前最常用的布局
+        </a-collapse-panel>
+        <a-collapse-panel key="grid" header="grid布局">
+          grid布局是三维布局
+        </a-collapse-panel>
+      </a-collapse>
   </section>
 
 </template>
