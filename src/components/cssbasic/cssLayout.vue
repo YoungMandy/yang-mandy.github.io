6d82e6eb5a045dc2b6d1e8dde705e7d814889ff4
huili.yang <huili.yang@qq.com>
2023-07-12 17:26:28 +0800

    完成float布局的示例

--- src/components/cssbasic/cssLayout.vue
+++ src/components/cssbasic/cssLayout.vue
@@ -49,6 +49,10 @@ const addItem = () => {
 const deleteItem = (index:number) => {
   tableFormList.splice(index, 1);
 }
+// ==== float布局 ====
+const float = reactive({
+  display:'inline-block'
+})
 </script>
 <template>
   <section class="component__css-layout">
@@ -63,6 +67,7 @@ const deleteItem = (index:number) => {
       </ul>
     </p>
       <a-collapse v-model:activeKey="activeKey">
+
         <a-collapse-panel key="table" header="table布局" >
           table布局在等高拖拽的时候有妙用,此处拖拽用到了
           <a 
@@ -117,7 +122,25 @@ const deleteItem = (index:number) => {
         </a-collapse-panel>
 
         <a-collapse-panel key="float" header="div + float布局" :forceRender="true">
-          div+ float布局
+          <ul>
+            <li> div+ float布局一般用在某个按钮需要居右,左边内容区占完的情况,在flex布局出现之前蛮常用的</li>
+            <li>默认情况下(没有使用flex或者grid布局),块状元素独占行,即使是宽度不满整行</li>
+          </ul>
+         
+          <a-form class="control-form" layout="inline" >
+            <a-form-item label="内容区域的display值:">
+              <a-radio-group v-model:value="float.display" button-style="solid">
+                <a-radio-button value="inline-block">inline-block</a-radio-button>
+                <a-radio-button value="block">block</a-radio-button>
+              </a-radio-group>
+            </a-form-item>
+          </a-form>
+
+          <div class="layout__float">
+            <div class="layout__float-content" :style="{display:float.display}">内容块1</div>
+            <div class="layout__float-content" :style="{display:float.display}">内容块2</div>
+            <a-button class="layout__float-button">我是浮动在右侧的按钮</a-button>
+          </div>
         </a-collapse-panel>
 
         <a-collapse-panel key="flex" header="flex布局">
@@ -248,5 +271,22 @@ $blue:#1890ff;
   cursor:not-allowed;
   opacity: .5;
   pointer-events: none;
+}
+
+// float布局
+.layout__float{
+  height:100px;
+  padding:10px;
+  background-color: blanchedalmond;
+  &-content{
+    height: 100%;
+    background:gainsboro;
+    display: inline-block;
+    border:1px solid greenyellow;
+    width:200px;
+  }
+  &-button{
+    float:right;
+  }
 }
 </style>
